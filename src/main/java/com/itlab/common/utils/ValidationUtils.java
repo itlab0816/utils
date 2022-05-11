4214d007971988b7ecae89feb27a2bee561f370c
ml <minloLee@sk.com>
2022-05-11 18:32:13 +0900

    고도화 테스트 추가

--- src/main/java/com/itlab/common/utils/ValidationUtils.java
+++ src/main/java/com/itlab/common/utils/ValidationUtils.java
@@ -15,22 +15,18 @@ public class ValidationUtils {
 
     private static final Logger logger = LoggerFactory.getLogger(ValidationUtils.class);
 
-//    public enum Type {
-//        PHONE_NUMBER
-//    }
-//
-//    public boolean check(String param, Type type) {
-//        return false;
-//    }
-
     public static boolean checkPhoneNumber(String phoneNumber) {
         // 010, 011, 016, 017, 018, 019-0000-0000
         String phoneNumberReg = "(01[016789])-(\\d{3,4})-(\\d{4})";
-        Pattern pattern = Pattern.compile(phoneNumberReg);
-        boolean isMatch = pattern.matcher(phoneNumber).matches();
+
+//        Pattern pattern = Pattern.compile(phoneNumberReg);
+//        boolean isMatch = pattern.matcher(phoneNumber).matches();
+
+        boolean isMatch = Pattern.matches(phoneNumberReg, phoneNumber);
 
         if (!isMatch) {
-            logger.debug("{}", phoneNumber);
+            logger.debug("{} is not phoneNumber!", phoneNumber);
+            throw new IllegalArgumentException(String.format("%s is not phoneNumber!", phoneNumber));
         }
 
         return isMatch;
--- src/test/java/com/itlab/common/utils/ValidationUtilsTest.java
+++ src/test/java/com/itlab/common/utils/ValidationUtilsTest.java
@@ -1,17 +1,32 @@
 package com.itlab.common.utils;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidationUtilsTest {
 
     @Test
-    void checkPhoneNumber() {
-        ValidationUtils.checkPhoneNumber("010-4652-7403");
-        ValidationUtils.checkPhoneNumber("011-4652-7403");
-        ValidationUtils.checkPhoneNumber("012-4652-7403");
-        ValidationUtils.checkPhoneNumber("013-4652-7403");
-        ValidationUtils.checkPhoneNumber("014-4652-7403");
+    void 휴대폰번호_유효성체크() {
+        // given
+        String phoneNumber = "010-0000-0000";
+
+        // when
+        boolean result = ValidationUtils.checkPhoneNumber(phoneNumber);
+
+        // then
+        assertThat(result).isEqualTo(true);
+    }
+    @Test
+    void 휴대폰번호_유효성체크_실패() {
+        // given
+        String phoneNumber = "012-0000-0000";
+
+        // when
+        // then
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> ValidationUtils.checkPhoneNumber(phoneNumber));
+        assertThat(illegalArgumentException.getMessage()).isEqualTo(String.format("%s is not phoneNumber!", phoneNumber));
     }
 }
